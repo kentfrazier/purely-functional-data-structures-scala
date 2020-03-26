b3371d5cef4f7090af291d3f10881f14e531e740
Kent Frazier <kentfrazier@gmail.com>
2020-03-26 18:50:01 -0500

    Add redundant binary number implementation

--- src/main/scala/com/kentfrazier/purefunc/numeric/RedundantBinaryNumber.scala
+++ src/main/scala/com/kentfrazier/purefunc/numeric/RedundantBinaryNumber.scala
@@ -1,23 +1,26 @@
 package com.kentfrazier.purefunc.numeric
 
 /**
- * Dense representation of zeroless binary numbers, with digits in increasing order of significance
+ * Dense representation of redundant binary numbers, with digits in increasing order of significance
  *
- * See Chapter 9.2.2
+ * See Chapter 9.2.3
  */
-object ZerolessBinaryNumber {
+object RedundantBinaryNumber {
 
   sealed abstract class Digit
+  case object Zero extends Digit
   case object One extends Digit
   case object Two extends Digit
 
   type Nat = List[Digit]
   object Nat {
-    implicit case object ZerolessBinaryNumberBN extends BinaryNumber[Nat] {
+    implicit case object RedundantBinaryNumberBN extends BinaryNumber[Nat] {
 
       override def increment(n: Nat): Nat = n match {
         case Nil =>
           One :: Nil
+        case Zero :: tail =>
+          One :: tail
         case One :: tail =>
           Two :: tail
         case Two :: tail =>
@@ -29,10 +32,12 @@ object ZerolessBinaryNumber {
           throw BinaryNumber.IllegalSubtractionError
         case One :: Nil =>
           Nil
+        case Zero :: tail =>
+          One :: decrement(tail)
+        case One :: tail =>
+          Zero :: tail
         case Two :: tail =>
           One :: tail
-        case One :: tail =>
-          Two :: decrement(tail) // borrow
       }
 
       override def add(n1: Nat, n2: Nat): Nat = (n1, n2) match {
@@ -42,6 +47,10 @@ object ZerolessBinaryNumber {
           other
         case (head1 :: tail1, head2 :: tail2) =>
           (head1, head2) match {
+            case (Zero, other) =>
+              other :: add(tail1, tail2)
+            case (other, Zero) =>
+              other :: add(tail1, tail2)
             case (One, One) =>
               Two :: add(tail1, tail2)
             case (One, Two) | (Two, One) =>
@@ -50,11 +59,12 @@ object ZerolessBinaryNumber {
               Two :: increment(add(tail1, tail2))
           }
       }
+
       override val zero: Nat = Nil
 
       override def toInt(n: Nat): Int = {
         n.zipWithIndex
-          .map {
+          .collect {
             case (One, rank) =>
               1 << rank
             case (Two, rank) =>
